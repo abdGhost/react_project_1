e26170c257f2637c62923bc6ae6760ed3e63ab9a
Abdul Taher Khan <49634197+abdGhost@users.noreply.github.com>
2024-01-31 17:55:32 +0530

    first commit

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,45 @@
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept/CoreConcept";
+import TabButton from "./components/CoreConcept/TabButton";
+
+function App() {
+  function handleClick(selectedButton) {
+    console.log(selectedButton);
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onClick={() => handleClick("components")}>
+              Components
+            </TabButton>
+            <TabButton onClick={() => handleClick("jsx")}>JSX</TabButton>
+            <TabButton onClick={() => handleClick("props")}>Props</TabButton>
+            <TabButton onClick={() => handleClick("state")}>State</TabButton>
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
